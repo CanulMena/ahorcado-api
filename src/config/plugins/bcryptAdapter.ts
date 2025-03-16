dbf87e913a61bb63e0fd6e52c1df70152c9d9615
Gustavo Canul <gusssy12@gmail.com>
2025-03-15 19:52:00 -0600

    Add bcryptjs package

---
+++ src/config/plugins/bcryptAdapter.ts
@@ -0,0 +1,14 @@
+import { compare, genSaltSync, hashSync } from 'bcryptjs';
+
+export const bcryptAdapter = {
+
+  hash: (password: string) => {
+    const salt = genSaltSync(10);
+    return hashSync(password, salt);
+  },
+
+  compare: (password: string, hashed: string) => {
+    return compare(password, hashed);
+  }
+
+}
